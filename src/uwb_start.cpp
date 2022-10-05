032c184c3b500ce48a9b88c47b1aa59e698997c5
cwcoscar <cwcoscar@gmail.com>
2022-10-05 15:36:14 +0800

    repairing the bug of can not ctrl+C to close program

--- src/uwb_start.cpp
+++ src/uwb_start.cpp
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <uwb/uwb_raw.h>
 #include "ros/ros.h"
+#include <signal.h>
 
 pthread_mutex_t mutex;
 uwb::uwb_raw uwb_data;
@@ -15,7 +16,12 @@ struct arg_struct {
 
 using namespace mn::CppLinuxSerial;
 
-const std::vector<std::string> split(const std::string &str, const char &delimiter) {
+void Siginthandler(int sig){
+    std::cout << "ROS shutting down!" << std::endl;
+    ros::shutdown();
+}
+
+const std::vector<std::string> Split(const std::string &str, const char &delimiter) {
     std::vector<std::string> result;
     std::stringstream ss(str);
     std::string tok;
@@ -26,7 +32,7 @@ const std::vector<std::string> split(const std::string &str, const char &delimit
     return result;
 }
 
-void available_anchor(const std::string& message, uwb::uwb_raw& data){
+void Available_anchor(const std::string& message, uwb::uwb_raw& data){
     int a_c = std::stoi(message);
     a_c % 2 == 1 ? data.A0 = true : data.A0 = false;
     a_c % 4 > 1 ? data.A1 = true : data.A1 = false;
@@ -34,12 +40,12 @@ void available_anchor(const std::string& message, uwb::uwb_raw& data){
     a_c > 7 ? data.A3 = true : data.A3 = false;
 }
 
-double convert_distance(const std::string& message){
+double Convert_distance(const std::string& message){
     double result = std::stoi(message, nullptr, 16);
     return result/1000;
 }
 
-uwb::uwb_raw fill_in_topic(const std::vector<std::string> data) {
+uwb::uwb_raw Fill_in_topic(const std::vector<std::string> data) {
     uwb::uwb_raw result;
     struct timeval t_now;
     gettimeofday(&t_now,NULL);
@@ -47,21 +53,21 @@ uwb::uwb_raw fill_in_topic(const std::vector<std::string> data) {
     result.stamp.sec = t_now.tv_sec;
     result.stamp.nsec = t_now.tv_usec*1000;
     result.MID = data[0];
-    available_anchor(data[1], result);
-    result.distance_to_A0 = convert_distance(data[2]);
-    result.distance_to_A1 = convert_distance(data[3]);
-    result.distance_to_A2 = convert_distance(data[4]);
-    result.distance_to_A3 = convert_distance(data[5]);
+    Available_anchor(data[1], result);
+    result.distance_to_A0 = Convert_distance(data[2]);
+    result.distance_to_A1 = Convert_distance(data[3]);
+    result.distance_to_A2 = Convert_distance(data[4]);
+    result.distance_to_A3 = Convert_distance(data[5]);
     result.Tag_id = (int)data[9][1] - '0';
     return result;
 }
 
-static void *reader_handler(void *arguments)
+static void *Reader_handler(void *arguments)
 {
     std::cout << "\033[32m" << "ENTER UWB Reader thread.\n" << "\033[0m" << std::endl;
     struct arg_struct *args = (struct arg_struct *)arguments;
 
-    while (1) {
+    while (ros::ok()) {
         std::string readData;
         std::string message_raw = "";
         
@@ -71,13 +77,13 @@ static void *reader_handler(void *arguments)
             message_raw = message_raw + readData;
         }
 		pthread_mutex_unlock(&mutex);
-        std::vector<std::string> message_data = split(message_raw, ' ');
+        std::vector<std::string> message_data = Split(message_raw, ' ');
 
         // for(int i = 0; i < message_data.size(); i++){
         //     std::cout << "\033[33m" << message_data[i] << "\033[0m" << std::endl;
         // }
 
-        uwb_data = fill_in_topic(message_data);
+        uwb_data = Fill_in_topic(message_data);
 		std::cout << "\033[33m" << uwb_data << "\033[0m" << std::endl;
 
         args->arg2.publish(uwb_data);
@@ -87,6 +93,8 @@ static void *reader_handler(void *arguments)
 }
 
 int main(int argc, char **argv) {
+    signal(SIGINT, Siginthandler);
+
     pthread_t thread_reader;
 
     ros::init(argc, argv, "uwb");
@@ -104,8 +112,8 @@ int main(int argc, char **argv) {
     args.arg1 = serialPort;
     args.arg2 = pub;
 
-    if (pthread_create(&thread_reader, NULL, reader_handler, &args)) {
-        perror("could not create thread for reader_handler");
+    if (pthread_create(&thread_reader, NULL, Reader_handler, &args)) {
+        perror("could not create thread for Reader_handler");
         return -1;
     }
     if (pthread_detach(thread_reader) != 0) {
@@ -114,7 +122,7 @@ int main(int argc, char **argv) {
     }
 
     sleep(1); // 1s
-	while(1);
+	while(ros::ok());
 
 	// Close the serial port
 	serialPort.Close();
